68214da1c1dba58dfdf91f1a95ce68479d6d27f5
shseo <shseo@humaxdigital.com>
2018-09-18 14:51:29 +0900

    Add Ex.2. (Not complete)

---
+++ Example/Ex.2.EpgExampleLinearLayout/app/src/main/java/com/example/shseo/epgexample_linearlayout/Activity_main.java
@@ -0,0 +1,14 @@
+package com.example.shseo.epgexample_linearlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
